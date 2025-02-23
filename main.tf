10d6c2b4faa4acb3faa59ee690b81495d8d79093
Sujeetkushwaha14 <121584876+Sujeetkushwaha14@users.noreply.github.com>
2025-02-24 03:35:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "azurerm_linux_virtual_machine" "example" {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
- 
+
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
